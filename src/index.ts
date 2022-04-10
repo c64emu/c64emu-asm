98f3417b51564f83db85e05ce85eb00affc66d22
Andreas Schwenk <andi.schwenk@googlemail.com>
2022-04-10 17:44:34 +0200

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import * as asm from './asm';
+
+export interface AssembleResult {
+    error: boolean;
+    errorString: string;
+    machineCode: Uint8Array;
+    machineCodeAddress: number;
+    stringifiedCode: string;
+}
+
+export function assemble6502(src: string): AssembleResult {
+    const assembler = new asm.ASM_MOS6502();
+    if (assembler.assemble(src) == false)
+        return {
+            error: true,
+            errorString: assembler.getErrorString(),
+            machineCode: null,
+            machineCodeAddress: 0,
+            stringifiedCode: '',
+        };
+    else {
+        const mc = assembler.getMachineCode();
+        return {
+            error: false,
+            errorString: '',
+            machineCode: new Uint8Array(mc.bytes),
+            machineCodeAddress: mc.startAddress,
+            stringifiedCode: mc.toString({
+                includeAddress: true,
+                matchSourceCode: true,
+                includeSourceCode: true,
+                maxBytesPerRow: 8,
+            }),
+        };
+    }
+}
